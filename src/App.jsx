647a4b8502fa2b49a6ef02036cefaa6cfe1f5770
Nikita Peplov <npeplov@yandex.ru>
2024-03-02 14:45:30 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { ProducstList } from './components/products-list'
+
+function App() {
+
+  return (
+    <>
+      <ProducstList/>
+    </>
+  )
+}
+
+export default App
